abf6797bdaad06e865472502e6bf0e0e0b16986d
arshhh11 <113960199+arshhh11@users.noreply.github.com>
2023-07-11 10:39:11 +0530

    Add files via upload

---
+++ r1.R
@@ -0,0 +1,10 @@
+mat1<-matrix(c(1,2,3,4,5,6,7,8),nrow=4,ncol=2)
+mat2<-matrix(c(9,10,11,12,13,14,15,16),nrow=4,ncol=2)
+print(mat1)
+print(mat2)
+addit<-mat1+mat2
+subr<-mat1-mat2
+mult<-mat1*mat2
+print(addit)
+print(subr)
+print(mult)
